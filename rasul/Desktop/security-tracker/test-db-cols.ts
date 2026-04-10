f42f99d862bc98e919bc89274268c6957c01106a
abdulazizDevop <olimovabdulaziz464@gmail.com>
2026-04-10 09:17:13 +0500

    refactoring

--- rasul/Desktop/security-tracker/test-db-cols.ts
+++
@@ -1,7 +0,0 @@
-import { query } from './lib/db';
-async function test() {
-  const res = await query("SELECT column_name, data_type FROM information_schema.columns WHERE table_name = 'app_users'");
-  console.log(res.rows);
-  process.exit(0);
-}
-test();
